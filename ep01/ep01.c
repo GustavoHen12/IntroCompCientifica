d37ea7150a0e3a0e074c4c77d4ea4ad5b86278b4
GustavoHen12 <gustavobarbosa@alunos.utfpr.edu.br>
2021-09-29 19:37:29 -0300

    le entrada

--- ep01/ep01.c
+++ ep01/ep01.c
@@ -7,57 +7,52 @@
 /* Size of input buffer.  */
 #define BUFFER_SIZE 256
 
-/* Program is demonstrating use of GNU libmatheval library of procedures
-        for evaluating mathematical functions.  */
-int main(int argc, char **argv)
-{
-    char buffer[BUFFER_SIZE]; /* Input buffer.  */
-    int length;               /* Length of above buffer. */
-    void *f, *f_prim;         /* Evaluators for function and function derivative.  */
-    char **names;             /* Function variables names. */
-    int count;                /* Number of function variables. */
-    double x;                 /* Variable x value.  */
-    int i;                    /* Loop counter. */
-
-    /* Read function.  Function has to be over variable x, or result may
-          be undetermined.  Size of textual represenatation of function is
-          bounded here to 256 characters, in real conditions one should
-          probably use GNU readline() instead of fgets() to overcome this
-          limit.  */
-    printf("f(x) = ");
-    fgets(buffer, BUFFER_SIZE, stdin);
-    length = strlen(buffer);
-    if (length > 0 && buffer[length - 1] == '\n')
-        buffer[length - 1] = '\0';
-
-    /* Create evaluator for function.  */
-    f = evaluator_create(buffer);
+// Função para iniciar uma função
+// Recebe como parametro uma string e retorna um ponteiro para função
+void* inicia_funcao(char* funcao){
+    void* f = evaluator_create(funcao);
     assert(f);
 
-    /* Print variable names appearing in function. */
-    evaluator_get_variables(f, &names, &count);
-    printf("  ");
-    for (i = 0; i < count; i++)
-        printf("%s ", names[i]);
-    printf("\n");
+    return f;
+}
 
-    /* Create evaluator for function derivative and print textual
-          representation of derivative.  */
-    f_prim = evaluator_derivative_x(f);
-    printf("  f'(x) = %s\n", evaluator_get_string(f_prim));
+// Função para calcular a derivada de f
+// Esta recebe como parametro a função e retorna o ponteiro para derivada desta
+void* calcula_derivada(void* f){
+    return evaluator_derivative_x(f);
+}
 
-    /* Read variable x value.  */
-    printf("x = ");
-    scanf("%lf", &x);
+void leParametros(void **f, double *x, double *epsilon, int *max_iter){
+    char func[BUFFER_SIZE];
+    scanf("%s", func);
+    *f = inicia_funcao(func);
 
-    /* Calculate and print values of function and its derivative for given
-          value of x.  */
-    printf("  f(%g) = %g\n", x, evaluator_evaluate_x(f, x));
-    printf("  f'(%g) = %g\n", x, evaluator_evaluate_x(f_prim, x));
+    scanf("%lf", x);
+    scanf("%lf", epsilon);
+    scanf("%d", max_iter);
+}
+
+int main(int argc, char **argv)
+{
+    void *f, *f_der;
+    double x_inicial;
+    double epsilon;
+    int max_iter;
+    
+    leParametros(&f, &x_inicial, &epsilon, &max_iter);
+    
+    f_der = calcula_derivada(f);
+
+    printf ("f(x) = %s\n", evaluator_get_string (f));
+    printf ("f'(x) = %s\n", evaluator_get_string (f_der));
+    printf("x_0 = %lf\nepsilon = %lf \nmax_iter = %d \n\n", x_inicial, epsilon, max_iter);
+
+    // printf("  f(%g) = %g\n", x, evaluator_evaluate_x(f, x));
+    // printf("  f'(%g) = %g\n", x, evaluator_evaluate_x(f_der, x));
 
     /* Destroy evaluators.  */
     evaluator_destroy(f);
-    evaluator_destroy(f_prim);
+    evaluator_destroy(f_der);
 
     exit(EXIT_SUCCESS);
 }
