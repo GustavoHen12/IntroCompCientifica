d691ba2a5e00901b7a2a57a277ec1b41f56ffd84
Iago Floriano <iagomfloriano@gmail.com>
2021-11-01 00:00:21 -0300

    Melhor modularização do código

--- ep02/ep02.c
+++ ep02/ep02.c
@@ -11,13 +11,42 @@ typedef struct {
   int k; //Quantidade de diagonais
 } Diagonais;
 
+// Imprime vetor de doubles com 6 casas decimais
+void imprime_vetor(double *vetor, int n){
+  for (int i = 0; i < n; i++){
+    printf("[%0.6f] ", vetor[i]);
+  }
+  printf("\n");
+}
+
+// Aplica função da entrada no vetor passado, o indice do numero no vetor será o valor de x
+void aplica_func(double *vetor, int n){
+  void *f;
+  char func[INPUT_SIZE];
+  scanf("%[^\t\n]s", func);
+  getchar();
+  f = evaluator_create(func);
+
+  for (int i = 0; i < n; i++){
+    vetor[i] = evaluator_evaluate_x(f, i);
+  }
+}
+
+// Inicializa vetor de tamanho n
+// Aplica função da entrada em seus elementos, sendo x o indice do elemento
+void leTermosVetor(double **vetor, int n){
+  *vetor = malloc(n * sizeof(double));
+  aplica_func(*vetor, n);
+}
+
 // Inicia D com n diagonais sendo a maior de tamanho tam
+// Também aplica as funções dadas na entrada
 void inicializa_diagonais(Diagonais *D, int n, int k){
   D->M = (double **)malloc(k * sizeof(double *));
   int tamAtual = n - (k / 2);
   int incrTam = 1;
   for(int i=0; i < k; i++){
-    D->M[i] = (double *)malloc(tamAtual * sizeof(double));
+    leTermosVetor(&(D->M[i]), tamAtual);
     if (tamAtual == n) incrTam = -1;
     tamAtual += incrTam;
   }
@@ -30,35 +59,19 @@ void imprime_diagonais(Diagonais *D){
   int nMeio = D->k / 2;
   for (int i = 0; i < D->k; i++){
     int tamAtual = D->n - abs(i - nMeio);
-    for (int j = 0; j < tamAtual; j++){
-      printf("[%0.6f] ", D->M[i][j]);
-    }
-    printf("\n");
+    imprime_vetor(D->M[i], tamAtual);
   }
-}
-
-// Função para iniciar uma função
-// Recebe como parametro uma string e retorna um ponteiro para função
-void* inicia_funcao(char* funcao){
-    void* f = evaluator_create(funcao);
-    return f;
 }
 
 // Lê função de stdin e aplica nas diagonais
 // valor de x é igual ao indice do elemento na diagonal
 void funcoesStdin_diagonais(Diagonais *D){
   int nMeio = D->k / 2;
-  void *f;
   for (int i = 0; i < D->k; i++){
-    char func[INPUT_SIZE];
-    scanf("%[^\t\n]s", func);
-    getchar();
-    f = inicia_funcao(func);
-
+    printf("Vai aplicar func\n");
     int tamAtual = D->n - abs(i - nMeio);
-    for (int j = 0; j < tamAtual; j++){
-      D->M[i][j] = evaluator_evaluate_x(f, j);
-    }
+    printf("Tamanho diagonal: %d\n", tamAtual);
+    aplica_func(D->M[i], tamAtual);
   }
 }
 
@@ -68,30 +81,6 @@ void leParametrosDiagonais(Diagonais *D, int *n, int *k){
   scanf("%d %d", n, k);
   getchar();
   inicializa_diagonais(D, *n, *k);
-  funcoesStdin_diagonais(D);
-}
-
-// Inicializa vetor de tamanho n
-// Aplica função da entrada em seus elementos, sendo x o indice do elemento
-void leTermosIndependentes(double **independentes, int n){
-  void *f;
-  char func[INPUT_SIZE];
-  scanf("%[^\t\n]s", func);
-  getchar();
-  f = inicia_funcao(func);
-  *independentes = malloc(n * sizeof(double));
-
-  for (int i = 0; i < n; i++){
-    (*independentes)[i] = evaluator_evaluate_x(f, i);
-  }
-}
-
-// Imprime vetor de doubles com 6 casas decimais
-void imprime_independentes(double *independentes, int n){
-  for (int i = 0; i < n; i++){
-    printf("[%0.6f] ", independentes[i]);
-  }
-  printf("\n");
 }
 
 int main(){
@@ -99,10 +88,10 @@ int main(){
   double *independentes;
   int n, k;
   leParametrosDiagonais(&d, &n, &k);
-  leTermosIndependentes(&independentes, n);
+  leTermosVetor(&independentes, n);
   
   imprime_diagonais(&d);
-  imprime_independentes(independentes, n);
+  imprime_vetor(independentes, n);
 
   return 0;
 }
