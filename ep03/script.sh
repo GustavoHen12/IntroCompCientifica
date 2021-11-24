084c169486470625370c604950bfedca862d97f8
Iago Floriano <iagomfloriano@gmail.com>
2021-11-24 19:02:57 -0300

    Script roda make e move saidas para outra pasta

--- ep03/script.sh
+++ ep03/script.sh
@@ -1,11 +1,24 @@
 #!/bin/bash
 TAMANHOS_TESTE=( 10 32 50 64 100 128 200 250 256 300 400 512 600 1000 1024 2000 2048 3000 4096)
+DIR_INICIAL="$PWD"
 
 SRC="$PWD/sistema"
-ENTRADAS="$PWD/sistema/entradas-sistema"
+cd $SRC
+echo "Rodando Make"
+make
+make geraSL
+cd $DIR_INICIAL
+
+ENTRADAS="$SRC/entradas-sistema"
 [[ -d $ENTRADAS ]] ||  mkdir -p $ENTRADAS
 
+echo "Gerando Sistemas e Resultados"
 for i in "${TAMANHOS_TESTE[@]}"; do
   $SRC/geraSL $i > $ENTRADAS/$i.sistema
   $SRC/gaussJacobi-likwid $ENTRADAS/$i.sistema
 done
+
+echo "Movendo Saidas"
+SAIDAS="$SRC/saidas-sistema"
+[[ -d $SAIDAS ]] || mkdir -p $SAIDAS
+mv out_* $SAIDAS
